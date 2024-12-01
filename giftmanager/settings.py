dd8e93aeeee0d6c57510693ff52f2f1943d4d68a
Coolman4128 <75655087+Coolman4128@users.noreply.github.com>
2024-12-01 11:12:17 -0500

    Update settings.py

--- giftmanager/settings.py
+++ giftmanager/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ['*']  # Replace '*' with your domain name in production
+ALLOWED_HOSTS = ['.giftmanager.top']  # Replace '*' with your domain name in production
 
 
 # Application definition
